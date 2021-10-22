dd83449924d2632788270f4306202c43d65f8457
Anh Cao <anhcao563@gmail.com>
2021-10-22 12:51:41 +0300

    Initial Commit

---
+++ src/main/java/com/example/bookingReservation/BookingReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingReservationApplication.class, args);
+	}
+
+}
